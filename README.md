e7912f0b029bf36035bd0edc97d296e0ea1cc71e
BCOM\nduong <nam-duong.duong@b-com.com>
2021-04-13 16:19:47 +0200

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+# Sample-MapUpdate
+[![License](https://img.shields.io/github/license/SolARFramework/SolARModuleTools?style=flat-square&label=License)](https://www.apache.org/licenses/LICENSE-2.0)
+
+The samples demonstrate the map update task that updates a global map based on changes of local maps.
+
+The samples for map update are open-source, designed by [b<>com](https://b-com.com/en), under [Apache v2 licence](https://www.apache.org/licenses/LICENSE-2.0).
---
+++ runDebug.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+export REMAKEN_PKG_ROOT=~/.remaken
+echo "REMAKEN_PKG_ROOT=$REMAKEN_PKG_ROOT"
+
+# Update configuration files by replacing win-cl-1.1 by linux in module paths
+if [ -f "$PWD/$1_conf.xml" ]; then
+   sed -i 's/win-cl-14.1/linux-gcc/' $PWD/$1_conf.xml
+fi
+
+if [ -f "$PWD/$1_Processing_conf.xml" ]; then
+   sed -i 's/win-cl-14.1/linux-gcc/' $PWD/$1_Processing_conf.xml
+fi
+
+if [ -f "$PWD/$1_Producer_conf.xml" ]; then
+   sed -i 's/win-cl-14.1/linux-gcc/' $PWD/$1_Producer_conf.xml
+fi
+
+if [ -f "$PWD/$1_Viewer_conf.xml" ]; then
+   sed -i 's/win-cl-14.1/linux-gcc/' $PWD/$1_Viewer_conf.xml
+fi
+
+# include dependencies path to ld_library_path
+ld_library_path="./"
+if [ -f "$PWD/$1_conf.xml" ]; then
+	for modulePath in $(grep -o "\$REMAKEN_PKG_ROOT.*lib" $1_conf.xml)
+	do
+	   modulePath=${modulePath/"\$REMAKEN_PKG_ROOT"/${REMAKEN_PKG_ROOT}}
+	   if ! [[ $ld_library_path =~ "$modulePath/x86_64/shared/debug" ]]
+	   then
+		  ld_library_path=$ld_library_path:$modulePath/x86_64/shared/debug
+	   fi 
+	done
+fi
+
+if [ -f "$PWD/$1_Processing_conf.xml" ]; then
+	for modulePath in $(grep -o "\$REMAKEN_PKG_ROOT.*lib" $1_Processing_conf.xml)
+	do
+	   modulePath=${modulePath/"\$REMAKEN_PKG_ROOT"/${REMAKEN_PKG_ROOT}}
+	   if ! [[ $ld_library_path =~ "$modulePath/x86_64/shared/debug" ]]
+	   then
+		  ld_library_path=$ld_library_path:$modulePath/x86_64/shared/debug
+	   fi 
+	done
+fi
+
+if [ -f "$PWD/$1_Producer_conf.xml" ]; then
+	for modulePath in $(grep -o "\$REMAKEN_PKG_ROOT.*lib" $1_Producer_conf.xml)
+	do
+	   modulePath=${modulePath/"\$REMAKEN_PKG_ROOT"/${REMAKEN_PKG_ROOT}}
+	   if ! [[ $ld_library_path =~ "$modulePath/x86_64/shared/debug" ]]
+	   then
+		  ld_library_path=$ld_library_path:$modulePath/x86_64/shared/debug
+	   fi 
+	done
+fi
+
+if [ -f "$PWD/$1_Viewer_conf.xml" ]; then
+	for modulePath in $(grep -o "\$REMAKEN_PKG_ROOT.*lib" $1_Viewer_conf.xml)
+	do
+	   modulePath=${modulePath/"\$REMAKEN_PKG_ROOT"/${REMAKEN_PKG_ROOT}}
+	   if ! [[ $ld_library_path =~ "$modulePath/x86_64/shared/debug" ]]
+	   then
+		  ld_library_path=$ld_library_path:$modulePath/x86_64/shared/debug
+	   fi 
+	done
+fi
+
+echo "LD_LIBRARY_PATH=${ld_library_path} $1 $2"
+LD_LIBRARY_PATH=$ld_library_path $1 $2
+
+
