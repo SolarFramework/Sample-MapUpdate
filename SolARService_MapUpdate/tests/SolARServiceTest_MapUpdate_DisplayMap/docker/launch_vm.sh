843f3aa97678f61dd507f01a7a24ef6c095bebbe
Christophe Cutullic <christophe.cutullic@b-com.com>
2021-11-16 14:02:05 +0100

    Add new test project to display current map from MapUpdate service

---
+++ SolARService_MapUpdate/tests/SolARServiceTest_MapUpdate_DisplayMap/docker/launch_vm.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# Get Map Update Service URL from parameters
+if [ "$1" ]
+then
+    echo "Map Update Service URL = $1"
+else
+    echo "You need to give Map Update Service URL as parameter!"
+    exit 1
+fi
+
+# Set MapUpdate Service URL
+export MAPUPDATE_SERVICE_URL=$1
+
+# Set Display
+export DISPLAY=${DISPLAY}
+xhost local:docker
+
+# Set application log level
+# Log level expected: DEBUG, CRITICAL, ERROR, INFO, TRACE, WARNING
+export SOLAR_LOG_LEVEL=INFO
+
+docker rm -f solarservicemapupdatedisplaymapclient
+docker run -it -d -e DISPLAY -e MAPUPDATE_SERVICE_URL -e SOLAR_LOG_LEVEL -e "SERVICE_NAME=SolARServiceMapUpdateDisplayMapClient" -v /tmp/.X11-unix:/tmp/.X11-unix --net=host --log-opt max-size=50m -e "SERVICE_TAGS=SolAR" --name solarservicemapupdatedisplaymapclient artwin/solar/services/map-update-displaymap-client:latest
